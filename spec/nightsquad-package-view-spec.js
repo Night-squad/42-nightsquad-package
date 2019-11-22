db2e8865b80752a8c800c69817262688d1a49013
Leo MARTIN <lmartin@e2r12p19.42.fr>
2019-11-22 07:58:06 +0100

    init

---
+++ spec/nightsquad-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NightsquadPackageView from '../lib/nightsquad-package-view';
+
+describe('NightsquadPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
