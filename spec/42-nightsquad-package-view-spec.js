685b322d55cc21ad3974e02c5201ad021569eeac
Leo MARTIN <lmartin@e2r12p19.42.fr>
2019-11-22 07:05:19 +0100

    init

---
+++ spec/42-nightsquad-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import 42NightsquadPackageView from '../lib/42-nightsquad-package-view';
+
+describe('42NightsquadPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
